dae1038728d4665b6c2cf6d30fa638963f344879
Josh Brown <josh@joossh.com>
2014-11-15 07:05:46 +0000

    Updated dashboard to use icons

--- client/modules/dashboard/controllers/dashboardIndex.js
+++ client/modules/dashboard/controllers/dashboardIndex.js
@@ -1,29 +1,12 @@
-dashboard.controller('dashboardIndexController', ['$scope', function($scope){
+dashboard.controller('dashboardIndexController', ['$scope', 'camCanvas', function($scope, camCanvas){
 	// Grab elements, create settings, etc.
-	var video = document.getElementById("video"),
-		videoObj = { "video": true },
-		errBack = function(error) {
-			console.log("Video capture error: ", error.code); 
-		};
+	var videoOverlay = document.getElementById("video-overlay");
 
-	// Put video listeners into place
-	if(navigator.getUserMedia) { // Standard
-		navigator.getUserMedia(videoObj, function(stream) {
-			video.src = stream;
-			video.play();
-		}, errBack);
-	} else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
-		navigator.webkitGetUserMedia(videoObj, function(stream){
-			video.src = window.webkitURL.createObjectURL(stream);
-			video.play();
-		}, errBack);
-	}
-	else if(navigator.mozGetUserMedia) { // Firefox-prefixed
-		navigator.mozGetUserMedia(videoObj, function(stream){
-			video.src = window.URL.createObjectURL(stream);
-			video.play();
-		}, errBack);
-	}
+	camCanvas.setAttribute('width', '150%');
+	camCanvas.setAttribute('height', '150%');
+	camCanvas.id = 'video';
 
-	stackBlurCanvasRGBA( 'video', 0, 0, 400, 400, 50 );
+	videoOverlay.appendChild(camCanvas);
+
+	stackBlurCanvasRGBA( camCanvas.id, 0, 0, 400, 400, 50 );
 }]);
